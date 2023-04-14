8f99c9bace268d8af847306eb4dac3740ccbf0c9
yeahokyok <982466+yeahokyok@users.noreply.github.com>
2023-04-14 21:36:34 +0700

    log for local

--- config/settings/local.py
+++ config/settings/local.py
@@ -8,3 +8,26 @@ SECRET_KEY = env.str(
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "verbose",
+        }
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
