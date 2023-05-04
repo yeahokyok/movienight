0b7066b0fdaf84a0f2d59efe4cfe1a7743879631
yeahokyok <982466+yeahokyok@users.noreply.github.com>
2023-05-04 16:36:49 +0700

    TestGetOrCreateGenres

--- movienight/movies/tests/test_omdb_integration.py
+++ movienight/movies/tests/test_omdb_integration.py
@@ -29,3 +29,20 @@ class TestGetOrCreateGenres(TestCase):
         self.assertEqual(len(genre_names), Genre.objects.count())
         for genre in genres:
             self.assertIn(genre, created_genres)
+
+    def test_get_existing_and_create_new_genre(self):
+        new_genre_names = ["Action", "Sci-Fi"]
+        existing_genre_names = ["Comedy", "Drama"]
+        created_genres = [
+            Genre.objects.create(name=name) for name in existing_genre_names
+        ]
+        genre_names = new_genre_names + existing_genre_names
+
+        genres = list(get_or_create_genres(genre_names))
+
+        self.assertEqual(len(genre_names), len(genres))
+        self.assertEqual(len(genre_names), Genre.objects.count())
+
+        for genre in genres:
+            self.assertTrue(Genre.objects.filter(pk=genre.pk).exists())
+            self.assertIn(genre.name, genre_names)
